55e63c94c122583ba9f6ad7f8fa0bd37c0ff5314
dbquiroga <db.quiroga93@gmail.com>
2019-10-31 23:08:33 -0300

    Remove header

--- app/src/pages/about.jsx
+++ app/src/pages/about.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
-import { Header } from 'components';
 import { Layout, Container } from 'layouts';
 
 const About = center => (
   <Layout>
     <Helmet title={'About Page'} />
-    <Header title="About Page">Gatsby Tutorial Starter</Header>
     <Container center={center}>
       <h3>
         If you would like to build this site completely from scratch, you can
