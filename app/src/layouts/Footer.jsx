4d44765271447124d21f45d9622e650224baefb6
gimenafernandz <gbffernandz@gmail.com>
2019-11-02 14:53:33 -0300

    changes in footer

--- app/src/layouts/Footer.jsx
+++ app/src/layouts/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const Wrapper = styled.footer`
+  text-align: center;
   position: relative;
   padding-top: 2rem;
   bottom: 0;
@@ -10,25 +11,57 @@ const Wrapper = styled.footer`
   font-family: ${props => props.theme.fontFamily.body};
   font-weight: 500;
   @media (max-width: ${props => props.theme.breakpoints.s}) {
-    padding-top: 7rem;
+    padding-top: 0.5rem;
+  }
+  div{
+    
+    width: 100%;
+    display: flex;
+    @media (max-width: ${props => props.theme.breakpoints.s}) {
+    flex-direction: column;
+    }
   }
 `;
 
 const Text = styled.div`
+@media (max-width: ${props => props.theme.breakpoints.s}) {
+  flex-direction: row;
+  text-align:center;
+  padding:0;}
+  width:50%;
   margin: 0;
-  padding-bottom: 2rem;
+  padding-bottom: 0.5rem;
   text-align: center;
   color: ${props => props.theme.colors.white.light};
+  padding-${props=> props.align}: 15px;
+  text-align:${props=> props.align};
+  p{
+    
+    margin:0;
+    padding-left: 0.5rem;
+  }
+ 
 `;
+
 
 const Footer = () => (
   <Wrapper>
-    <Text>
-      <span>
-        Plug In -{' '}
-        <a href="https://pluginonline.net">Plug In</a>
-      </span>
+    <div>
+     <Text align="left">
+      <p>hola@pluginonline.net
+      <a href="https://www.linkedin.com/company/28828264/admin/" target="_blank" >Linkedin</a>
+      <a href="https://www.facebook.com/Plug-In-107508997300613/?eid=ARAR5GonqU34h0tyZ0uDXGVpTGdmvCHfCsNJAe2rlXpurpehPiZAXiNgUicDttNZmjvjcFJCUcO6evIZ" target="_blank">Facebook</a></p>
     </Text>
+    <Text align="right">
+    <p>hola@pluginonline.net
+      <a href="https://www.linkedin.com/company/28828264/admin/" target="_blank" >Linkedin</a>
+      <a href="https://www.facebook.com/Plug-In-107508997300613/?eid=ARAR5GonqU34h0tyZ0uDXGVpTGdmvCHfCsNJAe2rlXpurpehPiZAXiNgUicDttNZmjvjcFJCUcO6evIZ" target="_blank">Facebook</a></p>
+    </Text>
+  </div>
+  <span>
+  Plug In -{' '}
+  <a href="https://pluginonline.net">Plug In</a>
+  </span>
   </Wrapper>
 );
 export default Footer;
