b1dda7671e0f5ef7a02ec13c797c406c0524dfbc
dbquiroga <db.quiroga93@gmail.com>
2019-11-02 11:32:42 -0300

    Added about things

--- app/src/pages/about.jsx
+++ app/src/pages/about.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import { Layout, Container } from 'layouts';
+import logo from '../../static/logo/logo-plugin.png';
 
-const About = center => (
+
+const StyledImg = styled.img`{
+  display: flex;
+  }`;
+
+const CardStyle = styled.div`
+  color: red;
+`;
+
+const Card = ({image, name, description}) => (
+  <CardStyle>
+    <img src={logo}/>
+    <h1>Holaaa</h1>
+    <p>Esto es un p</p>
+  </CardStyle>
+);
+
+const About = center => (  
   <Layout>
     <Helmet title={'About Page'} />
     <Container center={center}>
-      <h3>
-        If you would like to build this site completely from scratch, you can
-        read the guide{' '}
-        <a href="https://justinformentin.com/gatsby-v2-guide">here.</a>
-      </h3>
+      
+      <Card/>
+      <Card/>
+    
     </Container>
+
   </Layout>
 );
 
